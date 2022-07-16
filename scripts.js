6dfb00cf92e8e4ba26632cce4650f4cb388daf20
AkinShakira <shakiraakinleye@gmail.com>
2022-07-16 18:42:36 +0100

    function to cLC TIP and total per person

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 'use strict';
 
-const totalBill = document.querySelector('.js-total-bill');
-const totalPeople = document.querySelector('.js-num-people');
+// // to be deleted
+// const bill = Number(document.querySelector(".js-bill").value);
+// const totalPeople = document.querySelector(".js-num-people").value;
+// 
+
 const tip5 = document.querySelector('.js-btn-5');
 const tip10 = document.querySelector('.js-btn-10');
 const tip15 = document.querySelector('.js-btn-15');
@@ -11,3 +14,31 @@ const customTip = document.querySelector('.js-btn-custom');
 const totalTip = document.querySelector('.js-total-tip-fig');
 const totalPerPerson = document.querySelector('.js-total-person-fig');
 const btnReset = document.querySelector(".js-btn-reset");
+
+
+
+function calcTip() {
+  const bill = Number(document.querySelector(".js-bill").value);
+  const totalPeople = Number(document.querySelector(".js-num-people").value);
+  const t = Number(this.value);
+  let totalTipAmt = (t / 100) * bill;
+  let totalBill = totalTipAmt + bill;
+  let billEach = totalBill / totalPeople;
+  totalTip.textContent = totalTipAmt.toFixed(2);
+  totalPerPerson.textContent = billEach.toFixed(2);
+
+
+  console.log(totalTipAmt, totalBill, billEach);
+};
+
+tip5.addEventListener('click', calcTip);
+tip10.addEventListener("click", calcTip);
+tip15.addEventListener("click", calcTip);
+tip25.addEventListener("click", calcTip);
+tip50.addEventListener("click", calcTip);
+
+
+
+
+
+// calcTip(5, 200);
