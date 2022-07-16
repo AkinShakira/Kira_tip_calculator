8a568c279496b9e7e70989a2b5a7035e9a1b414c
AkinShakira <shakiraakinleye@gmail.com>
2022-07-16 17:32:39 +0100

    variable creation for all dom elements

---
+++ scripts.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const totalBill = document.querySelector('.js-total-bill');
+const totalPeople = document.querySelector('.js-num-people');
+const tip5 = document.querySelector('.js-btn-5');
+const tip10 = document.querySelector('.js-btn-10');
+const tip15 = document.querySelector('.js-btn-15');
+const tip25 = document.querySelector('.js-btn-25');
+const tip50 = document.querySelector('.js-btn-50');
+const customTip = document.querySelector('.js-btn-custom');
+const totalTip = document.querySelector('.js-total-tip-fig');
+const totalPerPerson = document.querySelector('.js-total-person-fig');
+const btnReset = document.querySelector(".js-btn-reset");
